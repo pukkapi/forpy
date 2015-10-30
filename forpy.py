4d68f0cb4ae43a216d827708c45c6fcd5fa433a2
pukkapi <gareth_evans515@hotmail.co.uk>
2015-10-30 11:25:56 +0000

    File ingestion and uniq ip's function added

--- forpy.py
+++ forpy.py
@@ -16,11 +16,40 @@ forpy.py <capturefile>
 
 """
 
+
+
+#Importing the Capture file
+
+globcapture = 0
+
+def import_file(cap_file):
+  global globcapture
+  globcapture = pyshark.FileCapture(cap_file)
+  #print capture[0] #Test 
+
+#Print out all IP's in the capture file
+
+def list_ips(capture):
+  ip_list = []
+  for packet in capture:
+    ip_list.append(packet.ip.addr)
+  uniq_ip_list = list(set(ip_list))
+  print """
+=====================================
+  Unique IP's 
+====================================
+"""
+  for x in uniq_ip_list: #test
+    print x
+
+
 #Argument Parsing Section
 
-if len(sys.argv) < 2:
+if len(sys.argv) != 2:
   print_usage()
 else:
-  print sys.argv[1]  
-
+  cap_file = sys.argv[1]
+  print "Analysing Capture File: " + cap_file
+  import_file(cap_file)
+  list_ips(globcapture)
 
